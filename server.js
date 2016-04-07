a368e852f3f4568b3e5152a31b6b3a174cb8e0ee
ruhsam <ruhsam@gmail.com>
2016-04-06 18:34:55 -0700

    began laying paths for models

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 //Server side JS
+//Requiring express because node is legit!
 var express = require('express');
+//requiring model folder
+var db = require('./models');
+//making express required and run on app variable
+var app = express();
