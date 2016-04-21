98ea1174a68281d03bca907cf4f68751223f4e3c
ruhsam <ruhsam@gmail.com>
2016-04-21 07:06:57 -0700

    Cleaned up code

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 var mongoose = require("mongoose");
 //mongoose.connect("mongodb://localhost/project_1");
 mongoose.connect( process.env.MONGOLAB_URI ||
-                       process.env.MONGOHQ_URL ||
-                       "mongodb://localhost/project_1" );
+   process.env.MONGOHQ_URL ||
+   "mongodb://localhost/project_1" );
 
-module.exports.Entry = require('./entry');
-module.exports.Comment = require('./comment');
+   module.exports.Entry = require('./entry');
+   module.exports.Comment = require('./comment');
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
-//Server side JS
-//Requiring express because node is legit!
 var express = require('express');
-//making express required and run on app variable
 var app = express();
 var bodyParser=require('body-parser');
 var PORT=process.env.PORT || 3000;
-//requiring model folder
 var db = require('./models');
 // serve static files from public folder
 app.use(express.static(__dirname + '/public'));
@@ -23,7 +19,7 @@ app.get('/', function homepage (req, res) {
    res.sendFile(__dirname + '/view/index.html');
 });
 
-//json endpoints
+////////////Get from DB route//////////////////////
 app.get('/api/entry', function (req, res) {
    db.Entry.find(function(err, entry){
       if (err) { return console.log("index error: " + err); }
@@ -31,6 +27,7 @@ app.get('/api/entry', function (req, res) {
    });
 });
 
+////////////Get from DB route End//////////////////
 
 ///////////////Posting to DB route//////////////////
 app.post('/api/entry', function (req, res) {
@@ -44,8 +41,9 @@ app.post('/api/entry', function (req, res) {
       res.json(saved);
    });
 });
+/////////////////Posting to DB route end/////////////////
 
-///////////////Posting Comment to DB route//////////////////
+///////////////Posting Comment to DB route///////////////
 app.post('/api/comment', function (req, res) {
    console.log(req.body);
    db.Entry.findById(req.body.entryId, function(err, foundEntry){
@@ -63,20 +61,17 @@ app.post('/api/comment', function (req, res) {
 
 });
 
-///////////////Removing from DB route//////////////////
+///////////////Removing from DB route////////////////////
 
 app.delete('/api/entry/:id', function (req, res) {
-   // get entry id from url params (`req.params`)
-   //console.log('entry delete', req.params);
    var entryId = req.params.id;
-   //console.log(entryId);
-   // find the index of the entry we want to remove
    db.Entry.findOneAndRemove({ _id: entryId }, function (err, deletedEntry) {
       res.json(deletedEntry);
    });
 });
+///////////////Remove from DB Route End//////////////////
 
-///////////////Removing Comment from DB route//////////////////
+///////////////Removing Comment from DB route////////////
 
 app.delete('/api/comment/:id', function (req, res) {
    // get comment id from url
@@ -94,13 +89,13 @@ app.delete('/api/comment/:id', function (req, res) {
    }
    });
 });
+///////////////Remove comment from DB End////////////////
 
-///////////////Editing route////////////////
+///////////////Editing Entry Route///////////////////////
 app.put('/api/entry/:id',function (req, res) {
    console.log("Save Button Pressed");
    db.Entry.findById(req.params.id, function(err, foundEntry) {
       if(err) {
-         //console.log('db entry update error', err);
          console.log('not editing');
          res.sendStatus(404);
       } else {
@@ -120,10 +115,7 @@ app.put('/api/entry/:id',function (req, res) {
    });
 
 });
-//
-//});
-
-// listen on port 3000
+////////////////Editing Entry Route End////////////////////
 app.listen(PORT, function () {
    console.log('Express server is running on http://localhost:3000/');
 });
